a97ee51ef43ba952475f2a711a763b24a69f767f
Aditi643 <tripathiaditi643@gmail.com>
2024-09-06 21:08:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("search-btn").addEventListener("click", function () {
+    let query = document.getElementById("search-input").value;
+    alert("Searching for properties in: " + query);
+});
